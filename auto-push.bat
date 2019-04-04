44ff067022bb35b1f22498915496d5e73d0f8623
f472918106 <472918106@qq.com>
2019-04-04 20:37:06 +0800

    auto-push-commit

--- auto-push.bat
+++ auto-push.bat
@@ -1,6 +1,8 @@
 git add .
 git commit -m "auto-push-commit"
 git push -u origin master
-@echo f472918106
-@echo F@1997f621
+if %errorlevel% == 1 (
+echo.| f472918106
+echo.| F@1997f621
+)
 cmd /k echo
